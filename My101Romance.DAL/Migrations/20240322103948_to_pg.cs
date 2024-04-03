c4ccba01556aa9a90c4d944165a84dd3f7a0ae5a
dolbolesya <zylynzy@gmail.com>
2024-04-03 21:19:32 +0300

    idk

---
+++ My101Romance.DAL/Migrations/20240322103948_to_pg.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace My101Romance.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class to_pg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
